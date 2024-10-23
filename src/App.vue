bd6a2a540401aeb9eb162889fd8efcc5015ee73d
PoushnayaA <poushnaya@bk.ru>
2024-10-23 19:25:50 +0300

    Add Header-component

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14 mb-14">
+    <Header />
+    <!-- 1.44.00 -->
+  </div>
+</template>
+
+<style scoped></style>
