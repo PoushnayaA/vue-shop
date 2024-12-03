44a2a668041c18d031ba9cb809f7472295a12092
PoushnayaA <poushnaya@bk.ru>
2024-12-03 13:19:09 +0300

    add drawer

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,33 +1,79 @@
 <script setup>
+import axios from 'axios'
 import DrawerHead from './DrawerHead.vue'
 import CartItemList from './CartItemList.vue'
-</script>
+import InfoBlock from './InfoBlock.vue'
+//7.14
 
+defineProps({
+  totalPrice: Number,
+  vatPrice: Number,
+  isDisabledButton: Boolean,
+})
+
+const isLoadingOrder = ref(false)
+
+const emit = defineEmits(['createOrder'])
+
+const handleButtonClick = () => {
+  emit('createOrder')
+}
+
+const createOrder = async () => {
+  try {
+    isLoadingOrder.value = true
+    const { data } = await axios.post(
+      `https://0bc60d73538f2561.mokky.dev/orders`,
+      {
+        items: cart.value,
+        totalPrice: totalPrice.value,
+      },
+    )
+    cart.value = []
+    return data
+  } catch (err) {
+    console.log(err)
+  } finally {
+    isLoadingOrder.value = false
+  }
+}
+</script>
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
   <div
-    class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8 flex flex-col justify-between"
+    v-auto-animate
+    class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8 flex flex-col"
   >
     <DrawerHead />
-    <CartItemList />
+    <div v-if="!totalPrice" class="flex h-full items-center">
+      <InfoBlock
+        title="Корзина пустая"
+        description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ"
+        image-url="/package-icon.png"
+      />
+    </div>
+    <div v-else class="flex flex-col h-full">
+      <CartItemList />
 
-    <div class="flex flex-col gap-4 mt-7">
-      <div class="flex gap-2">
-        <span>Итого:</span>
-        <div class="flex-1 mb-1 border-b border-dashed"></div>
-        <b>12990 руб.</b>
+      <div class="flex flex-col gap-4 mt-7">
+        <div class="flex gap-2">
+          <span>Итого:</span>
+          <div class="flex-1 mb-1 border-b border-dashed"></div>
+          <b>{{ totalPrice }} руб.</b>
+        </div>
+        <div class="flex gap-2">
+          <span>Налог 5%:</span>
+          <div class="flex-1 mb-1 border-b border-dashed"></div>
+          <b>{{ vatPrice }} руб.</b>
+        </div>
+        <button
+          @click="handleButtonClick"
+          :disabled="isDisabledButton"
+          class="mt-4 bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 transition"
+        >
+          Оформить заказ
+        </button>
       </div>
-      <div class="flex gap-2">
-        <span>Налог 5%:</span>
-        <div class="flex-1 mb-1 border-b border-dashed"></div>
-        <b>12990 руб.</b>
-      </div>
-      <button
-        disabled
-        class="mt-4 bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 transition"
-      >
-        Оформить заказ
-      </button>
     </div>
   </div>
 </template>
