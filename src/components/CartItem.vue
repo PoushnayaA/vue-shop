ff06d74a5de529d58ef78bd095b6732bfa99c631
PoushnayaA <poushnaya@bk.ru>
2024-12-03 13:19:25 +0300

    add cart

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,18 +1,28 @@
-<script></script>
+<script setup>
+const emit = defineEmits(['onClickRemove'])
+defineProps({
+  id: Number,
+  title: String,
+  imageUrl: String,
+  price: Number,
+})
+</script>
 
 <template>
   <div class="border border-slate-200 p-4 rounded-xl flex items-center gap-4">
-    <img class="w-16 h-16" src="/sneakers/sneakers-3.jpg" alt="Sneaker" />
+    <img class="w-16 h-16" :src="imageUrl" :alt="title" />
     <div class="flex flex-col w-full">
-      <p>Кроссвочки nike</p>
+      <p>{{ title }}</p>
 
       <div class="flex justify-between mt-2">
-        <b>12990 руб</b>
-        <img
-          class="opacity-40 hover:opacity-100 cursor-pointer transition"
-          src="/close.svg"
-          alt="Удалить"
-        />
+        <b class="flex-1">{{ price }}</b>
+        <button @click="emit('onClickRemove')">
+          <img
+            class="opacity-40 hover:opacity-100 cursor-pointer transition"
+            src="/close.svg"
+            alt="Удалить"
+          />
+        </button>
       </div>
     </div>
   </div>
